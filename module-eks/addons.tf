1728d0f479f9fae0ee559aba5138f45782220c7c
inijoy <inijo1@gmail.com>
2025-11-30 23:29:32 +0100

    Deployment

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -26,22 +26,25 @@ data "aws_eks_cluster_auth" "eks" {
     name = aws_eks_cluster.eks.name
 }
 resource "helm_release" "nginx_ingress" {
-    name       = "nginx-ingress"
-    repository = "https://kubernetes.github.io/ingress-nginx"
-    chart      = "ingress-nginx"
-    version    = "4.14.0"
-    namespace  = "ingress-nginx"
-    create_namespace = true
+  name             = "nginx-ingress"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = "4.14.0"
+  namespace        = "ingress-nginx"
+  create_namespace = true
 
-    values = [file("${path.module}/nginx-ingress-values.yaml")]
-    depends_on = [ aws_eks_node_group.eks_node_group]
+  wait    = true
+  timeout = 600
+
+  values = [file("${path.module}/nginx-ingress-values.yaml")]
+
+  depends_on = [aws_eks_node_group.eks_node_group]
 }
 
 data "aws_lb" "nginx_ingress" {
   tags = {
-    "kubernetes.io/service-name" = "ingress-nginx/nginx-ingress-ingress-nginx-controller"
+    "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
   }
-
   depends_on = [helm_release.nginx_ingress]
 }
 
